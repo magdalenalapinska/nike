c7f1226d30cbdbc72da0a7241fcc3ae724722700
magdalenalapinska <magdalena.lapinska21@gmail.com>
2024-12-23 18:10:25 +0100

    Add components folder and prepare its starting files, create index.js, add import lines to both App.jsx and index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./assets/components";
+
 const App = () => (
   <main className="relative">
-    Nav
-    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-    <section className="padding">Popular Products</section>
-    <section className="padding">Super Quality</section>
-    <section className="padding">Special Offer</section>
-    <section className="bg-pale-blue padding">Customer Reviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    {/* <Nav></Nav> */}
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero></Hero>
+    </section>
+    <section className="padding">
+      <PopularProducts></PopularProducts>
+    </section>
+    <section className="padding">
+      <SuperQuality></SuperQuality>
+    </section>
+    <section className="padding">
+      <SpecialOffer></SpecialOffer>
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews></CustomerReviews>
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe></Subscribe>
+    </section>
     <section className="bg-black padding-x padding-t pb-8 text-white">
-      Footer
+      <Footer></Footer>
     </section>
   </main>
 );
