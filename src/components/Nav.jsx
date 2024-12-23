1e74bcbb95ca5f3c3044711bc69c5a2a8cbde47f
magdalenalapinska <magdalena.lapinska21@gmail.com>
2024-12-23 22:13:31 +0100

    Add nav links and a hamburger menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
@@ -8,6 +9,21 @@ const Nav = () => {
         <a href="/">
           <img src={headerLogo} alt="logo" width={130} height={29} />
         </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item, index) => (
+            <li key={index}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
       </nav>
     </header>
   );
