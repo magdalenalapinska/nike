def3dd05f36d9b43cb51c17fed436034d1ebd92c
magdalenalapinska <magdalena.lapinska21@gmail.com>
2024-12-23 21:58:27 +0100

    Change components and sections folders directory, add Nav.jsx and its styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ import {
   SpecialOffer,
   Subscribe,
   SuperQuality,
-} from "./assets/components";
+} from "./sections";
+import Nav from "./components/Nav";
 
 const App = () => (
   <main className="relative">
-    {/* <Nav></Nav> */}
+    <Nav></Nav>
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero></Hero>
     </section>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import { hamburger } from "../assets/icons";
+import { headerLogo } from "../assets/images";
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="logo" width={130} height={29} />
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
