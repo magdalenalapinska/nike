de9aa9b79d9639136fdc45527a13513bfa190a27
magdalenalapinska <magdalena.lapinska21@gmail.com>
2024-12-27 19:34:11 +0100

    Create section and main heading for Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,23 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-4 border-red-500 p-2 bg-slate-800 "
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="bg-slate-200">Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike </span>Shoes
+        </h1>
+        <p>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
