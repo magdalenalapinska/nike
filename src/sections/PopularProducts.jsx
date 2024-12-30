4e4e1585450566e9852caca9731b92b27a3d08da
magdalenalapinska <magdalena.lapinska21@gmail.com>
2024-12-30 23:31:52 +0100

    Import products and create section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,5 +1,13 @@
+import { products } from "../constants";
+
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2>Popular products</h2>
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
